baf7eedce91a811474da87202eb38b755dccbd3e
Michael Tharpe <mtharpe@gmail.com>
2025-01-21 01:48:03 -0500

    updated for lint

--- variables.tf
+++ variables.tf
@@ -1,6 +1,6 @@
 variable "resource_group_name" {
   description = "The name of the resource group in which the resources will be created"
-  type = string
+  type        = string
 }
 
 variable "vnet_subnet_id" {
@@ -10,58 +10,58 @@ variable "vnet_subnet_id" {
 
 variable "public_ip_dns" {
   description = "Optional globally unique per datacenter region domain name label to apply to each public ip address. e.g. thisvar.varlocation.cloudapp.azure.com where you specify only thisvar here. This is an array of names which will pair up sequentially to the number of public ips defined in var.nb_public_ip. One name or empty string is required for every public ip. If no public ip is desired, then set this to an array with a single empty string."
-  type = string
+  type        = string
 }
 
 variable "admin_password" {
   description = "The admin password to be used on the VMSS that will be deployed. The password must meet the complexity requirements of Azure"
-  type = string
+  type        = string
 }
 
 variable "ssh_key" {
   description = "Path to the public key to be used for ssh access to the VM.  Only used with non-Windows vms and can be left as-is even if using Windows vms. If specifying a path to a certification on a Windows machine to provision a linux vm use the / in the path versus backslash. e.g. c:/home/id_rsa.pub"
   default     = "~/.ssh/id_rsa.pub"
-  type = string
+  type        = string
 }
 
 variable "remote_port" {
   description = "Remote tcp port to be used for access to the vms created via the nsg applied to the nics."
-  type = string
+  type        = string
 }
 
 variable "admin_username" {
   description = "The admin username of the VM that will be deployed"
   default     = "azureuser"
-  type = string
+  type        = string
 }
 
 variable "custom_data" {
   description = "The custom data to supply to the machine. This can be used as a cloud-init for Linux systems."
-  type = string
+  type        = string
 }
 
 variable "storage_account_type" {
   description = "Defines the type of storage account to be created. Valid options are Standard_LRS, Standard_ZRS, Standard_GRS, Standard_RAGRS, Premium_LRS."
   default     = "Premium_LRS"
-  type = string
+  type        = string
 }
 
 variable "vm_size" {
   description = "Specifies the size of the virtual machine."
   default     = "Standard_D2s_v3"
-  type = string
+  type        = string
 }
 
 variable "nb_instances" {
   description = "Specify the number of vm instances"
   default     = "1"
-  type = number
+  type        = number
 }
 
 variable "vm_hostname" {
   description = "local name of the VM"
   default     = "myvm"
-  type = string
+  type        = string
 }
 
 variable "vm_os_simple" {
@@ -71,34 +71,34 @@ variable "vm_os_simple" {
 
 variable "vm_os_id" {
   description = "The resource ID of the image that you want to deploy if you are using a custom image.Note, need to provide is_windows_image = true for windows custom images."
-  type = string
+  type        = string
 }
 
 variable "is_windows_image" {
   description = "Boolean flag to notify when the custom image is windows based."
   default     = false
-  type = bool
+  type        = bool
 }
 
 variable "vm_os_publisher" {
   description = "The name of the publisher of the image that you want to deploy. This is ignored when vm_os_id or vm_os_simple are provided."
-  type = string
+  type        = string
 }
 
 variable "vm_os_offer" {
   description = "The name of the offer of the image that you want to deploy. This is ignored when vm_os_id or vm_os_simple are provided."
-  type = string
+  type        = string
 }
 
 variable "vm_os_sku" {
   description = "The sku of the image that you want to deploy. This is ignored when vm_os_id or vm_os_simple are provided."
-  type = string
+  type        = string
 }
 
 variable "vm_os_version" {
   description = "The version of the image that you want to deploy. This is ignored when vm_os_id or vm_os_simple are provided."
   default     = "latest"
-  type = string
+  type        = string
 }
 
 variable "tags" {
@@ -113,13 +113,13 @@ variable "tags" {
 variable "allocation_method" {
   description = "Defines how an IP address is assigned. Options are Static or Dynamic."
   default     = "Dynamic"
-  type = string
+  type        = string
 }
 
 variable "nb_public_ip" {
   description = "Number of public IPs to assign corresponding to one IP per vm. Set to 0 to not assign any public IP addresses."
   default     = "1"
-  type = number
+  type        = number
 }
 
 variable "delete_os_disk_on_termination" {
@@ -131,13 +131,13 @@ variable "delete_os_disk_on_termination" {
 variable "data_sa_type" {
   description = "Data Disk Storage Account type"
   default     = "Standard_LRS"
-  type = string
+  type        = string
 }
 
 variable "data_disk_size_gb" {
   description = "Storage data disk size size"
   default     = 30
-  type = number
+  type        = number
 }
 
 variable "boot_diagnostics" {
@@ -149,7 +149,7 @@ variable "boot_diagnostics" {
 variable "boot_diagnostics_sa_type" {
   description = "(Optional) Storage account type for boot diagnostics"
   default     = "Standard_LRS"
-  type = string
+  type        = string
 }
 
 variable "enable_ssh_key" {
@@ -161,5 +161,5 @@ variable "enable_ssh_key" {
 variable "nb_data_disk" {
   description = "(Optional) Number of the data disks attached to each virtual machine"
   default     = 0
-  type = number
+  type        = number
 }
